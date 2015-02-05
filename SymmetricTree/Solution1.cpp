7e54e51037ca01e1eb0fed88b639b1fa1efb3ab5
brhythm <brhythm@gmail.com>
2015-02-04 22:19:01 -0500

    Fixed symmetric tree

--- SymmetricTree/Solution1.cpp
+++ SymmetricTree/Solution1.cpp
@@ -7,7 +7,8 @@
  */
 
 #include <stddef.h>
-#include <deque>
+#include <stack>
+
 using namespace std;
 
 struct TreeNode {
@@ -20,74 +21,51 @@ struct TreeNode {
 
 class Solution {
 public:
-    //Iterative solution 
+    //Iterative solution using stack
     bool isSymmetric(TreeNode *root) {
         if (root == nullptr)
         {
             return true;
         }
-        deque<TreeNode*> current;//FIFO
-        deque<TreeNode*> next;
-        current.push_back(root);
-        bool isTreeSymmetric = true;
 
-        while(!current.empty())
-        {
-            while(!current.empty())
-            {
-                TreeNode* node = current.front();//everything from queue is not null
-                current.pop_front();
-                if (node->left != nullptr)
-                {
-                    next.push_back(node->left);
-                }
-                if (node->right != nullptr)
-                {
-                    next.push_back(node->right);
-                }
+        stack<TreeNode*> nodeStack;
+        nodeStack.push(root->left);
+        nodeStack.push(root->right);
+        while (!nodeStack.empty()) {
+            TreeNode* right = nodeStack.top();
+            nodeStack.pop();
+            TreeNode* left = nodeStack.top();
+            nodeStack.pop();
+            if (left == nullptr &&
+                right == nullptr){
+                //both children are null, OK
+                ;
             }
-            //finish generating queue for next level
-            if (!isLevelSymmetric(next))
-            {
-                isTreeSymmetric= false;
-                break;
-            }
-            swap(next, current);
-        }
-        return isTreeSymmetric;
-   }
-
-    bool isLevelSymmetric(const deque<TreeNode*>& next)
-    {
-        int size = next.size();
-        if (size == 0)
-        {
-            return true;
-        }
-        if (size%2 != 0)
-        {
-            return false;
-        }
-        int i = 0;
-        int j = size-1;//must be odd number
-        while ( i < j)
-        {
-            if (next[i]->val != next[j]->val)
-            {
+            else if (left == nullptr || right == nullptr) {
                 return false;
             }
-            ++i;
-            --j; 
+            else if (left->val != right->val) {
+                //both pointer not null
+                return false;
+            }
+            else
+            {//left ,right values are equal
+                nodeStack.push(right->right);
+                nodeStack.push(left->left);
+                
+                nodeStack.push(right->left);
+                nodeStack.push(left->right);
+            }
         }
         return true;
     }
+    
 };
 //Idea: One straightforward way is to level order traverse the tree, and 
 //for each level, check whether the nodes in the queues are symmetric or not
-//However, queues don't have iterator. We chose to use deque here. Even thought
-//we don't need to insert/delete at the end, deque has random access iterator
-//(similar interface to vector) and allow us to check symmetric easily
-// deque is usually implemented as dynamic array
+//However, this algorithm is NOT always correct. e.g. return wrong value for {1,2,2,#,3,#,3}
+//
+//Use a stack to replace the recursive alg.
 
 
 
