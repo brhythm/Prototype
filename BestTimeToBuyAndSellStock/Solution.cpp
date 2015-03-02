4d6b23f525eb36a4e6c28d4ffb36c5a5bce0b0cc
brhythm <brhythm@gmail.com>
2015-03-01 20:26:49 -0500

    Update 2 files

--- BestTimeToBuyAndSellStock/Solution.cpp
+++ BestTimeToBuyAndSellStock/Solution.cpp
@@ -1,48 +1,34 @@
-/*
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * 
- * If you were only permitted to complete at most one transaction
- *  (ie, buy one and sell one share of the stock), 
- * design an algorithm to find the maximum profit.
- * 
+/**
+ *Say you have an array for which the ith element is the price of a given stock on day i.
+ *If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
  */
 #include <vector>
+#include <climits>
 using namespace std;
 
 class Solution {
-public:
+public://Time O(N), Space O(1)
     int maxProfit(vector<int> &prices) {
-    	//Running time O(N), space O(1)
-    	if ( prices.size() > 0 )
-    	{
-    		int minIndex = 0;
-    		int maxIndex = 0;
-    	
-    		int maxProfit = prices[maxIndex] - prices[minIndex];
-    		for ( int index = 1; index < prices.size(); ++index )
-    		{
-    			if (prices[index] < prices[minIndex] )
-    			{// update minIndex, must refresh maxIndex too
-    			minIndex = index;
-					maxIndex = index;
-				}
-				else if ( prices[index]> prices[maxIndex] )
-				{
-					maxIndex = index;
-				}
+        int maxProfit = 0;
+        int low = INT_MAX;
+        int high = INT_MIN;
 
-				int localMaxProfit = prices[maxIndex] - prices[minIndex];
-				if ( localMaxProfit> maxProfit )
-				{
-					maxProfit = localMaxProfit;
-				}
-			}
-			return maxProfit;    
-    	}
-    	else
-    	{
-    		return 0;
-    	}
+        for (int i=0; i < prices.size(); ++i)
+        {
+            if (prices[i] < low)
+            {
+                low = prices[i];
+                high = prices[i];
+                maxProfit = max(maxProfit, high-low);
+            }
+            if (prices[i] > high)
+            {
+                high = prices[i];
+                maxProfit = max(maxProfit, high-low);
+            }
+        }
+        return maxProfit;
     }
 };
+
 
--- BestTimeToBuyAndSellStockIII/Solution.cpp
+++ BestTimeToBuyAndSellStockIII/Solution.cpp
@@ -20,42 +20,39 @@ using namespace std;
 
 class Solution {
 public:
-	static int maxProfit(vector<int> &prices) {
+	int maxProfit(vector<int> &prices) {
 		//Running time O(N), space O(N)
-		if (prices.size() > 0) {
-			//leftVector[i] = maxProfit between 0 ~ i;
-			vector<int> leftVector(prices.size(), 0);
-			//rightVector[i] = maxProfit between i~n-1
-			vector<int> rightVector(prices.size(), 0);
-			
-			int localMin = prices[0];
-			//leftVector[0]=0;
-			for (int i=1; i< prices.size(); ++i)
-			{
-				localMin = min(prices[i], localMin);
-				leftVector[i] = max(leftVector[i-1], prices[i]-localMin); 
-			}
-			
-			int localMax = prices[prices.size()-1];
-			//rightVector[n-1]=0
-			for (int i=prices.size()-2; i>=0; --i)
-			{
-				localMax = max(prices[i], localMax);
-				rightVector[i] = max(rightVector[i+1], localMax-prices[i]);
-			}
-			
-			int maxProfit = 0;
-			for(int i=0; i < prices.size(); ++i)
-			{
-				maxProfit = max(maxProfit, leftVector[i]+rightVector[i]);
-			}
-			
-			return maxProfit;
-		}
-		else
-		{
-			return 0;
-		}
+	    if (prices.size() == 0)
+	    {
+	        return 0;
+	    }
+	    int n=prices.size();
+	    vector<int> left(n, 0);//max profit in [0, i-1] with at most one transaction
+	    vector<int> right(n, 0);//max profit in [i, n-1], with at most one transaction
+	    //To calc left or right, we already know that it took O(N) to calc local max in length-N array
+	    //So without any optimization, we could filled up left in O(N^2)
+	    //However, we could use DP to memorize the subproblems we have solved easily, and
+	    //filling up left array could be optimized to O(N)
+	    int localMin = prices[0];
+	    for (int i=1; i < n; ++i)
+	    {
+	        localMin = min(localMin, prices[i]);
+	        left[i] = max(left[i-1], prices[i]-localMin);
+	    }
+	    localMin = prices[n-1];
+	    int localMax = prices[n-1];
+	    //right[n-1] = 0
+	    for (int i=n-2; i >=0; --i)
+	    {
+	        localMax = max(localMax, prices[i]);
+	        right[i] = max(right[i+1], localMax-prices[i]);
+	    }
+	    int maxProfit =0;
+	    for (int i=0; i < n; ++i)
+	    {
+	        maxProfit = max(maxProfit, left[i]+right[i]);
+	    }
+	    return maxProfit;
 	}
 };
 /*
@@ -68,55 +65,5 @@ int main()
 	
 	cout << Solution::maxProfit(testVector) << endl;
 	
-}
-*/
-
-/*
-	for (int i=1; i < prices.size(); ++i) {
-		// for each D[i]
-		
-		maxProfitVector[i] = maxProfitVector[i-1];
-		//currentMax = D[i-1] 
-		numTransactions[i] = numTransactions[i-1];
-		//default numTransaciton is numTransaction[i-1], if no transaction occurred
-		
-		//if ( prices[i] > prices[i-1])
-		//{
-		int localMin = prices[i-1];// localMin price among [x, i-1]
-		
-		int lastPossibleDay = i-1;
-		while (numTransactions[lastPossibleDay] ==2)
-			lastPossibleDay--;
-		
-		//lastPossibleDay might have 1 or 0 transactions
-		for (int x=lastPossibleDay; x >=0; --x )
-		{// Note that we try all possible D[x] in descending order
-			if (prices[x] < localMin)
-			{
-				localMin = prices[x];
-			}
-
-			int diff = prices[i] - localMin;
-			if ( (maxProfitVector[x] + diff) >=  maxProfitVector[i] ) 
-			//WARNING MUST USE >=, we want to minimize the numTransactions
-			{
-				maxProfitVector[i] = maxProfitVector[x] + diff;
-				numTransactions[i] = numTransactions[x] + 1;
-			}
-				
-		}
-		
-		//}
-	}		
-
-	
-	for (int i=0; i< prices.size(); ++i)
-	{
-		cout << maxProfitVector[i] << "," << numTransactions[i] << endl;
-	}
-	return maxProfitVector[prices.size()-1];
-	
-} else {
-	return 0;
 }
 */
