b930406514983bf287aef8a75af87cb1fe0f14e2
brhythm <brhythm@gmail.com>
2015-04-14 23:02:55 -0400

    Add more comments

--- GasStation/Solution.cpp
+++ GasStation/Solution.cpp
@@ -36,13 +36,25 @@ public:
     	}
     	else
     	{
+		//How to get the starting index
+		//The start station we found, the accumulated sum starting from this position 
+		//MUST BE ALWAYS POSITIVE until S[n], if sum at any following station becomes negative,
+	    //it means starting from here would FAIL to finish the trip.
+		//Then what ? If the final solution is index i
+	    //Observe that sum of Delta[0]...Delta[i-1] must be negative
+	    // and sum of Delta[i]...Delta[n] must be positive
+	    // 
+		// When we found a FAIL, we simply found a sequence which sums up to a negative number, the final answer will NOT fall into it
+        // so we can safely try starting from next index (reset sum = 0), and see whether we could reach Delta[n] with sum being always positive
     		sum = 0;
 			int startIndex = 0;
 			for (int i = 0; i < numStation; ++i) {
 				sum += diff[i];
 				if (sum < 0) {
-					startIndex = i + 1;
-					sum = 0;
+					//FAILED, remaining gas cannot cover the diff at this station
+					//old startIndex is a bad starting point, but right answer will could be exluded from that old range
+					startIndex = i + 1;//try next possible startIndex
+					sum = 0;		   //reset sum
 				}
 			}
 			return startIndex;
